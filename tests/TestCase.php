5e784c1a06bea5ff6266020bd662f62d2a39d9d9
AbdullahWael-Dev <abdallahwael352@gmail.com>
2026-06-12 21:51:00 +0300

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,13 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Override;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutVite();
+    }
 }
